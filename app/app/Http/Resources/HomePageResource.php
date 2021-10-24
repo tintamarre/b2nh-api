5bb3201afd35b12a3040c8c3616a0ae245f60447
Martin Erpicum <martin@erpicum.net>
2021-10-24 20:06:03 +0200

    🍺improve navigation menu

--- app/app/Http/Resources/HomePageResource.php
+++ app/app/Http/Resources/HomePageResource.php
@@ -11,12 +11,27 @@ class HomePageResource extends BaseResource
         return [
             'title' => env('APP_NAME'),
       
-            'links' => [
-                'self' => URL::current(),
-                'volcanoes' => route('api.volcanoes.index'),
-                'volcano_events' => route('api.volcano_events.index'),
-                'tsunami_events' => route('api.tsunami_events.index'),
-                'earthquake_events' => route('api.earthquake_events.index')
+            'navigation_menu' => [
+                [
+                    'title' => 'self',
+                    'url' => URL::current()
+                ],
+                [
+                    'title' => 'volcanoes',
+                    'url' => route('api.volcanoes.index'),
+                ],
+                [
+                    'title' => 'volcano_events',
+                    'url' => route('api.volcano_events.index'),
+                ],
+                [
+                    'title' => 'tsunami_events',
+                    'url' => route('api.tsunami_events.index'),
+                ],
+                [
+                    'title' => 'earthquake_events',
+                    'url' => route('api.earthquake_events.index'),
+                ]
             ],
             ];
     }
